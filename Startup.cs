30c12bdd5a67c2133fd2ebaf5107f03516544134
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-08-18 09:15:35 +0200

    Allowed easier passwords (during testing)

--- Startup.cs
+++ Startup.cs
@@ -48,6 +48,7 @@ namespace IdentityApp
                     options.Password.RequireDigit = false;
                     options.Password.RequireLowercase = false;
                     options.Password.RequireUppercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
                     options.SignIn.RequireConfirmedAccount = true;
                 })
                 .AddEntityFrameworkStores<IdentityDbContext>();
