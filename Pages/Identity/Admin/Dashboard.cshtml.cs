33fa080cfa36ca88c4a034c5092eac7e08426072
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-08-18 08:08:44 +0200

    Used IdentityUser and UserManager

--- Pages/Identity/Admin/Dashboard.cshtml.cs
+++ Pages/Identity/Admin/Dashboard.cshtml.cs
@@ -1,11 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
 namespace IdentityApp.Pages.Identity.Admin
 {
     public class DashboardModel : AdminPageModel
     {
+        public DashboardModel(UserManager<IdentityUser> userMgr)
+            => UserManager = userMgr;
+        public UserManager<IdentityUser> UserManager { get; set; }
+
         public int UsersCount { get; set; } = 0;
         public int UsersUnconfirmed { get; set; } = 0;
         public int UsersLockedout { get; set; } = 0;
         public int UsersTwoFactor { get; set; } = 0;
+
+        private readonly string[] emails = {
+            "alice@example.com", "bob@example.com", "charlie@example.com"
+        };
+        public async Task<IActionResult> OnPostAsync()
+        {
+            foreach (string email in emails)
+            {
+                IdentityUser userObject = new IdentityUser
+                {
+                    UserName = email,
+                    Email = email,
+                    EmailConfirmed = true
+                };
+                IdentityResult result = await UserManager.CreateAsync(userObject);
+                result.Process(ModelState);
+            }
+
+            if (ModelState.IsValid)
+            {
+                return RedirectToPage();
+            }
+            return Page();
+        }
     }
 }
