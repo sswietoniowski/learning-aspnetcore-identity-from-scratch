799b14a4e8e0bafc425105a890a482b89f30a91f
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-08-18 08:00:09 +0200

    Created custom dashboard

---
+++ Pages/Identity/UserPageModel.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace IdentityApp.Pages.Identity
+{
+    public class UserPageModel : PageModel
+    {
+        // no methods or properties required
+    }
+}
